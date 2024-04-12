d3c44c2ade2a2768ca48605386218f5716941469
Joanerson Costa <joanersonc31@gmail.com>
2024-04-12 18:11:43 -0300

    inicia projeto

---
+++ src/main/java/dev/wakandaacademy/UsuarioPostApplication.java
@@ -0,0 +1,13 @@
+package dev.wakandaacademy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsuarioPostApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsuarioPostApplication.class, args);
+	}
+
+}
