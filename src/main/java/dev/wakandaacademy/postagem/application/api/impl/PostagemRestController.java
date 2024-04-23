3e32fe05365ac2d39c08fe49be4a6c9202daf620
Joanerson Costa <joanersonc31@gmail.com>
2024-04-23 12:52:54 -0300

    implementa getUsuarioByToken no PostagemRestController

--- src/main/java/dev/wakandaacademy/postagem/application/api/impl/PostagemRestController.java
+++ src/main/java/dev/wakandaacademy/postagem/application/api/impl/PostagemRestController.java
@@ -1,7 +1,10 @@
 package dev.wakandaacademy.postagem.application.api.impl;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import dev.wakandaacademy.config.security.service.TokenService;
+import dev.wakandaacademy.handler.APIException;
 import dev.wakandaacademy.postagem.application.api.PostagemAPI;
 import dev.wakandaacademy.postagem.application.api.request.PostagemRequest;
 import dev.wakandaacademy.postagem.application.api.response.PostagemIdResponse;
@@ -12,6 +15,7 @@ import lombok.extern.log4j.Log4j2;
 @RequiredArgsConstructor
 @Log4j2
 public class PostagemRestController implements PostagemAPI {
+	private final TokenService tokenService;
 
 	@Override
 	public PostagemIdResponse criaPostagem(String token, PostagemRequest postagemRequest) {
@@ -20,4 +24,13 @@ public class PostagemRestController implements PostagemAPI {
 		return null;
 	}
 
+	private String getUsuarioByToken(String token) {
+		log.info("[inicia] ChamadoRestController - getUsuarioByToken");
+		log.debug("[token] {}", token);
+		String emailUsuario = tokenService.getUsuarioByBearerToken(token)
+				.orElseThrow(() -> APIException.build(HttpStatus.UNAUTHORIZED, token));
+		log.info("[emailUsuario] {}", emailUsuario);
+		log.info("[finaliza] ChamadoRestController - getUsuarioByToken");
+		return emailUsuario;
+	}
 }
