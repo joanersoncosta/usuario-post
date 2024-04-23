8d302a282f7b69cc19ff00f6cc803585e104d278
Joanerson Costa <joanersonc31@gmail.com>
2024-04-23 09:36:04 -0300

    implementa getUsuarioByToken no ConteudoRestCOntroller

--- src/main/java/dev/wakandaacademy/conteudo/application/api/ConteudoRestController.java
+++ src/main/java/dev/wakandaacademy/conteudo/application/api/ConteudoRestController.java
@@ -1,9 +1,12 @@
 package dev.wakandaacademy.conteudo.application.api;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import dev.wakandaacademy.config.security.service.TokenService;
 import dev.wakandaacademy.conteudo.application.api.request.ConteudoRequest;
 import dev.wakandaacademy.conteudo.application.api.response.ConteudoIdResponse;
+import dev.wakandaacademy.handler.APIException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 
@@ -12,11 +15,24 @@ import lombok.extern.log4j.Log4j2;
 @Log4j2
 public class ConteudoRestController implements ConteudoAPI {
 
+	private final TokenService tokenService;
+
 	@Override
 	public ConteudoIdResponse criaConteudo(String token, ConteudoRequest ConteudoRequest) {
 		log.info("[inicia] ConteudoRestController - criaConteudo");
 		log.info("[finaliza] ConteudoRestController - criaConteudo");
 		return null;
 	}
+	
+	private String getUsuarioByToken(String token) {
+		log.info("[inicia] ChamadoRestController - getUsuarioByToken");
+		log.debug("[token] {}", token);
+		String emailUsuario = tokenService.getUsuarioByBearerToken(token)
+				.orElseThrow(() -> APIException.build(HttpStatus.UNAUTHORIZED, token));
+		log.info("[emailUsuario] {}", emailUsuario);
+		log.info("[finaliza] ChamadoRestController - getUsuarioByToken");
+		return emailUsuario;
+	}
+
 
 }
